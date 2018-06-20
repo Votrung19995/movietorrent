f45860985af49f6b14fd420d281381f005424ae4
Vo Le Minh Trung <Votrung017@gmail.com>
2018-06-20 20:43:24 +0700

    add more fix gui

--- blogtorrent/resources/views/search.blade.php
+++ blogtorrent/resources/views/search.blade.php
@@ -3,7 +3,7 @@
            <div class="container">
 				<div class="row">
 						<div class="col-md-4">
-								<form style="margin-top: 11px;">
+								<form style="margin-top: 15px;">
 										<div class="input-group" style="margin-right: 0px;padding: 0px;border-radius: 20px; width: 100%">
 												<input type="text" class="form-control" style="background: transparent;border: 1px solid #AFB0B7;border-top-left-radius: 20px;border-bottom-left-radius: 20px " placeholder="Nhập từ khóa cần tìm">
 												<span class="input-group-btn" style="margin-right: 0px">
@@ -13,16 +13,16 @@
 								</form>
 						</div>
 						<div class="col-md-2">
-						    <h5 style="text-align: center; margin-top: 12px; color: #3f51b5 ! important"><a href="#" class="hov" style="color: #3f51b5;opacity: 0.9"><img src="{{asset('img/h5-new.png')}}" style="height: 40px; width: 40px; display: inline; margin:0px" class="img-responsive"> PHIM  MỚI</a></h5>
+						    <h5 style="text-align: center; margin-top: 15px; color: #3f51b5 ! important"><a href="#" class="hov" style="color: #3f51b5;opacity: 0.9"><img src="{{asset('img/h5-new.png')}}" style="height: 40px; width: 40px; display: inline; margin:0px" class="img-responsive"> PHIM  MỚI</a></h5>
 						</div>
 
 						<div class="col-md-2">
-						    <h5 style="text-align: center; margin-top: 12px; color: #887568 ! important"><a href="#" class="hov" style="color: #3f51b5;opacity: 0.9"><img src="{{asset('img/h2-top.png')}}" style="height: 40px; width: 40px; display: inline; margin:0px" class="img-responsive"> TOP IMDB 2018</a></h5>
+						    <h5 style="text-align: center; margin-top: 15px; color: #887568 ! important"><a href="#" class="hov" style="color: #3f51b5;opacity: 0.9"><img src="{{asset('img/h2-top.png')}}" style="height: 40px; width: 40px; display: inline; margin:0px" class="img-responsive"> TOP IMDB 2018</a></h5>
 						</div>
 
 						
 						<div class="col-md-3">
-						    <h5 style="text-align: center; margin-top: 12px; color: #887568 ! important"><a href="#" class="hov" style="color: #3f51b5;opacity: 0.9"><img src="{{asset('img/h2-hay.png')}}" style="height: 40px; width: 40px; display: inline; margin:0px" class="img-responsive"> PHIM HAY TUYỂN CHỌN</a></h5>
+						    <h5 style="text-align: center; margin-top: 15px; color: #887568 ! important"><a href="#" class="hov" style="color: #3f51b5;opacity: 0.9"><img src="{{asset('img/h2-hay.png')}}" style="height: 40px; width: 40px; display: inline; margin:0px" class="img-responsive"> PHIM HAY TUYỂN CHỌN</a></h5>
 						</div>
 
 						<div class="col-md-1"></div>
